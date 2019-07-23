57ea1f48276e5893f20a0bedc633895112f33feb
Saksham <gupta.10@iitj.ac.in>
2019-07-23 22:48:55 +0530

    url change

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -23,7 +23,6 @@ class EchoView(views.APIView):
 
 urlpatterns = [
     url(r'^$', generic.RedirectView.as_view(url='/api/', permanent=False)),
-
     url(r'^api/$', get_schema_view()),
     url(r'^api/auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^api/auth/token/obtain/$', TokenObtainPairView.as_view()),
